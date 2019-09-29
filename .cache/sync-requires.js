9a55b3c8b92003abb590f0e7e9c13519c4742e4d
Michał Kokosiński <19owen91@gmail.com>
2019-09-30 00:43:03 +0200

    Testing: added md templates

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\.cache\\dev-404-page.js"))),
+  "component---src-components-recipes-recipe-template-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\components\\Recipes\\RecipeTemplate.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\404.js"))),
   "component---src-pages-app-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\app.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\index.js")))
--- gatsby-config.js
+++ gatsby-config.js
@@ -32,32 +32,32 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `recipes`,
-    //     path: `${__dirname}/src/pages/recipes`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `recipes`,
+        path: `${__dirname}/src/pages/recipes`,
+      },
+    },
     "gatsby-transformer-remark",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/*`] },
+      options: { prefixes: [`/app/*`] },
     },
     {
       resolve: 'gatsby-source-firestore',
