6e4bc9dac17abb765d24f76cd0cf1ef8f039f122
Michał Kokosiński <19owen91@gmail.com>
2019-09-29 23:15:43 +0200

    after build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\index.js"))),
   "component---src-pages-recipes-js": hot(preferDefault(require("C:\\Programowanie\\kuchmistrz-gatsby\\src\\pages\\Recipes.js"))),
