7585b2bbcfe4bdea75cdaca6812d64a00d7d8a67
Michał Kokosiński <19owen91@gmail.com>
2020-03-13 22:14:15 +0100

    added array fields for steps and ingredients

--- src/components/Forms/AddRecipeForm.js
+++ src/components/Forms/AddRecipeForm.js
@@ -1,17 +1,34 @@
 import React, { useState } from "react"
-import { Formik, useFormik } from "formik"
+import { Formik, Form, Field, FieldArray } from "formik"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import { uploadImg, addRecipe } from "../../services/api"
 
 import * as MultistepForm from "./MultistepForm"
 
-let stepsCounter = 1;
+const StepInput = ({ name, formikProps }) => {
+  return (
+    <div className="field">
+      <label className="label" htmlFor="name">
+        {name}
+      </label>
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="Jaki jest kolejny krok?"
+          name={name}
+          {...formikProps}
+        />
+      </div>
+    </div>
+  )
+}
+
+let stepsCounter = 1
 
 export const AddRecipeForm = () => {
-  const [steps, setSteps] = useState(['Krok 1'])
-
   const onSubmitHandler = (values, { setSubmitting }) => {
-    console.log("submit", formik.values)
+    console.log("submit", values)
     const { name, desc, img } = values
     // uploadImg(img)
     //   .then(uploadedPath => {
@@ -29,120 +46,186 @@ export const AddRecipeForm = () => {
     //   })
   }
 
-  const formik = useFormik({
-    initialValues: { name: "", desc: "", img: "" },
-    onSubmit: onSubmitHandler,
-  })
-
-  const StepInput = ({name}) => {
-    return (
-      <div className="field">
-        <label className="label" htmlFor="name">
-          {name}
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Jaki jest kolejny krok?"
-            name={name}
-            {...formik.getFieldProps(name)}
-          />
-        </div>
-      </div>
-    )
-  }
-
-  const addStep = () =>{
-    setSteps([...steps, 'Krok ' + ++stepsCounter])
-  }
-
   return (
     <div className="card">
       <div className="card-content">
-        <form onSubmit={formik.handleSubmit}>
-          <MultistepForm.Form>
+        <Formik
+          initialValues={{
+            name: "",
+            desc: "",
+            img: "",
+            steps: [{ name: "step1", label: "Krok 1", value: "" }],
+            ingredients: [
+              { name: "ingredient", label: "Składnik 1", value: "" },
+            ],
+          }}
+          onSubmit={onSubmitHandler}
+        >
+          {({ values, setFieldValue }) => (
+            <Form>
+              <MultistepForm.Form>
+                <MultistepForm.Step step={1}>
+                  <div className="field">
+                    <label className="label" htmlFor="name">
+                      Nazwa
+                    </label>
+                    <div className="control">
+                      <Field
+                        className="input"
+                        type="text"
+                        placeholder="Nazwa przepisu"
+                        name="name"
+                      />
+                    </div>
+                  </div>
 
+                  <div className="field">
+                    <label className="label">Opis</label>
+                    <div className="control">
+                      <Field
+                        type="textarea"
+                        className="textarea"
+                        placeholder="Krótki opis przepisu"
+                        name="desc"
+                      />
+                    </div>
+                  </div>
 
-            <MultistepForm.Step step={1}>
-              <div className="field">
-                <label className="label" htmlFor="name">
-                  Nazwa
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Nazwa przepisu"
-                    name="name"
-                    {...formik.getFieldProps("name")}
-                  />
-                </div>
-              </div>
+                  <div className="file">
+                    <label className="file-label">
+                      <Field
+                        className="file-input"
+                        type="file"
+                        name="img"
+                        value={values.img.path}
+                        onChange={event => {
+                          setFieldValue("img", event.currentTarget.files[0])
+                        }}
+                      />
+                      <span className="file-cta">
+                        <span className="file-icon">
+                          <i className="fas fa-upload"></i>
+                        </span>
+                        <span className="file-label">Dodaj zdjęcie</span>
+                      </span>
+                    </label>
+                  </div>
+                </MultistepForm.Step>
 
-              <div className="field">
-                <label className="label">Opis</label>
-                <div className="control">
-                  <textarea
-                    className="textarea"
-                    placeholder="Krótki opis przepisu"
-                    name="desc"
-                    {...formik.getFieldProps("desc")}
-                  ></textarea>
-                </div>
-              </div>
+                <MultistepForm.Step step={2}>
+                  <FieldArray name="steps">
+                    {({ push, remove }) => (
+                      <div>
+                        {values.steps.map((step, index) => (
+                          <>
+                          <label className="label" htmlFor="name">
+                              {step.label}
+                            </label>
+                            <div className="field is-grouped" key={index}>
+                              <div className="control is-expanded">
+                              <Field
+                                className="input"
+                                type="text"
+                                placeholder="Jaki jest kolejny krok?"
+                                name={`steps[${index}].value`}
+                              />
+                            </div>
+                            {values.steps.length === index + 1 &&
+                                index > 0 && (
+                                  <div className="control">
+                                    <button
+                                      type="button"
+                                      className="button"
+                                      onClick={() => {
+                                        remove(index)
+                                      }}
+                                    >
+                                      -
+                                    </button>
+                                  </div>
+                                )}
+                          </div>
+                          </>
+                        ))}
+                        <button
+                          type="button"
+                          className="button"
+                          onClick={() =>
+                            push({
+                              name: `step${values.steps.length + 1}`,
+                              label: `Krok ${values.steps.length + 1}`,
+                              value: "",
+                            })
+                          }
+                        >
+                          +
+                        </button>
+                      </div>
+                    )}
+                  </FieldArray>
+                </MultistepForm.Step>
 
-              <div className="file">
-                <label className="file-label">
-                  <input
-                    className="file-input"
-                    type="file"
-                    name="img"
-                    value={formik.values.img.path}
-                    onChange={event => {
-                      formik.setFieldValue("img", event.currentTarget.files[0])
-                    }}
-                  />
-                  <span className="file-cta">
-                    <span className="file-icon">
-                      <i className="fas fa-upload"></i>
-                    </span>
-                    <span className="file-label">Dodaj zdjęcie</span>
-                  </span>
-                </label>
-              </div>
-            </MultistepForm.Step>
+                <MultistepForm.Step step={3}>
+                  Składniki:
+                  <FieldArray name="ingredients">
+                    {({ push, remove }) => (
+                      <>
+                        {values.ingredients.map((ingredient, index) => (
+                          <>
+                            <label className="label" htmlFor="name">
+                              {ingredient.label}
+                            </label>
+                            <div className="field is-grouped">
+                              <div className="control is-expanded">
+                                <input
+                                  className="input"
+                                  type="text"
+                                  placeholder="Nazwa przepisu"
+                                  name={`ingredients.${index}.value`}
+                                />
+                              </div>
+                              {values.ingredients.length === index + 1 &&
+                                index > 0 && (
+                                  <div className="control">
+                                    <button
+                                      type="button"
+                                      className="button"
+                                      onClick={() => {
+                                        remove(index)
+                                      }}
+                                    >
+                                      -
+                                    </button>
+                                  </div>
+                                )}
+                            </div>
+                          </>
+                        ))}
+                        <button
+                          type="button"
+                          className="button"
+                          onClick={() =>
+                            push({
+                              name: `ingredient${values.ingredients.length +
+                                1}`,
+                              label: `Składnik ${values.ingredients.length +
+                                1}`,
+                              value: "",
+                            })
+                          }
+                        >
+                          +
+                        </button>
+                      </>
+                    )}
+                  </FieldArray>
+                </MultistepForm.Step>
 
-
-            <MultistepForm.Step step={2}>
-              {steps.map(step => <StepInput name={step} />)}
-              <div className="button" onClick={addStep}>+</div>
-            </MultistepForm.Step>
-
-
-
-            <MultistepForm.Step step={3}>
-              Składniki:
-              <div className="field">
-                <label className="label" htmlFor="name">
-                  Składnik 1
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Nazwa przepisu"
-                    name="ingedient1"
-                    {...formik.getFieldProps("ingedient1")}
-                  />
-                </div>
-              </div>
-            </MultistepForm.Step>
-
-
-            <MultistepForm.Buttons />
-          </MultistepForm.Form>
-        </form>
+                <MultistepForm.Buttons />
+              </MultistepForm.Form>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   )
