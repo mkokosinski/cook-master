78e56cc4389628c227ff1bf6947963d58ea266d5
Michał Kokosiński <19owen91@gmail.com>
2020-04-22 13:23:45 +0200

    added csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,22 @@ module.exports = {
     "gatsby-plugin-sharp",
     `gatsby-plugin-sass`,
     {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        // disableOnDev: true,
+        // reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        // mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        // mergeStyleHashes: true, // you can disable styles sha256 hashes
+        // mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self'",
+          "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self'"
+          // you can add your directives or override defaults
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-env-variables`,
       options: {
         whitelist: ["MY_VAR", "MY_OTHER_VAR"],
